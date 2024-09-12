d81b8891f44bc677f6942151f74188f044311095
Bryan Cabrera <133478274+NohBodie@users.noreply.github.com>
2024-09-12 01:12:53 +0000

    Testing Thu Sep 12 2024 01:12:53 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex01/student/main.cpp
+++ chapter2/ex01/student/main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main() {
     // Write your main here
+    cout << "**********************************" << endl;
+    cout << "                                  " << endl;
+    cout << "*    Programming Assignment 1    *" << endl;
+    cout << "                                  " << endl;
+    cout << "*     Computer Programming I     *" << endl;
+    cout << "                                  " << endl;
+    cout << "*     Author: Bryan Cabrera      *" << endl;
+    cout << "                                  " << endl;
+    cout << "*   Due Date: Thursday, Jan. 24  *" << endl;
+    cout << "                                  " << endl;
+    cout << "**********************************" << endl;
+
     return 0;
+
 }
