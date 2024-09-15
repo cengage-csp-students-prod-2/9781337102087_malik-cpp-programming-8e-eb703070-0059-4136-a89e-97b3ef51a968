de3eca8002f1b83cee463c8f1a996751d456ac12
Bryan Cabrera <133478274+NohBodie@users.noreply.github.com>
2024-09-15 20:53:55 +0000

    Testing Sun Sep 15 2024 20:53:55 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -21,7 +21,11 @@ average = 0;
 
 average = num1 + num2 + num3 / 3;
 
-cout << num1, num2, num3, average;
+cout << "num1: " << num1 << ", ";
+cout << "num2: " << num2 << ", ";
+cout << "num3: " << num3 << ", ";
+cout << "average: " << average << endl;
+
 
 return 0;
 }
