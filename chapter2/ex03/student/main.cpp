f04cc8bce07ef02e5a5cad26c2072abd652e996c
Bryan Cabrera <133478274+NohBodie@users.noreply.github.com>
2024-09-15 20:56:19 +0000

    Testing Sun Sep 15 2024 20:56:19 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -15,7 +15,7 @@ int average = 0;
 
 //executable statements
 
-average = num1 + num2 + num3 / 3;
+average = (num1 + num2 + num3) / 3;
 
 cout << num1 << ", " << num2 << ", " << num3 << ", " << average << endl;
 
