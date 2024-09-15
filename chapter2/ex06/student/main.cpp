f3d814fe623fcab28c14e94906312f31c9731b02
Bryan Cabrera <133478274+NohBodie@users.noreply.github.com>
2024-09-15 22:14:54 +0000

    Testing Sun Sep 15 2024 22:14:54 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/main.cpp
+++ chapter2/ex06/student/main.cpp
@@ -9,12 +9,14 @@ int main() {
 string name;
 double studyHours;
 //executable statements
-cout << "Name: " << name;
+cout << "Name: ";
 cin >> name;
 
-cout << "Study Hours: " << studyHours;
+cout << "Study Hours: ";
 cin >> studyHours;
 
+cout << "Hello, " << name << "!" << " On Saturday, you need to study " << studyHours << " for the exam.";
 //return statement
+
 return 0;
 }
