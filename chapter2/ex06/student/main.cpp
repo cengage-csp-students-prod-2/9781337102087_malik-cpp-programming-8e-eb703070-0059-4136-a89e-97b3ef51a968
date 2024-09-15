e4a4e9f47dd7b1d139c0d07218e73aa6e6dfedbb
Bryan Cabrera <133478274+NohBodie@users.noreply.github.com>
2024-09-15 21:59:20 +0000

    Testing Sun Sep 15 2024 21:59:20 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/main.cpp
+++ chapter2/ex06/student/main.cpp
@@ -3,14 +3,21 @@
 //using namespace statement
 using namespace std;
 
-
-int main()
-{
+int main() {
 //variable declaration
-char name
-double studyHours
+char name;
+double studyHours;
+
 //executable statements
+cout << "Name: " << name;
+cin >> name;
+
+cout << "Study Hours: " << studyHours;
+cin >> studyHours;
+
 
 
 //return statement
+
+return 0;
 }
