74dbd6e58a832b4ea95b8ff0e8d51004bf8613dd
Bryan Cabrera <133478274+NohBodie@users.noreply.github.com>
2024-09-15 18:31:43 +0000

    Testing Sun Sep 15 2024 18:31:43 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.cpp
+++ chapter2/ex03/student/main.cpp
@@ -1,16 +1,31 @@
 //include statement(s)
+#include <iostream>
+
 
 //using namespace statement
-
+using namespace std;
 
 int main()
 {
 
 //variable declaration
-
+int num1;
+num1 = 125;
+int num2;
+num2 = 28;
+int num3;
+num3 = -25;
+int average;
+average = 0;
 
 //executable statements
 
+average = num1 + num2 + num3 / 3;
 
-//return 0;
+cout << average;
+
+
+
+
+return 0;
 }
